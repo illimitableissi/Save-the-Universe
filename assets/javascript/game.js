461852384903a791811d2b3ab9f9d2bade320c0a
SOVEREIGN <isimemeni@gmail.com>
2019-09-14 19:27:29 -0400

    added more functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,47 @@
-var targetNumber = 120
-console.log(targetNumber);
+var targetNumbers = [40, 55, 79, 120, 20, 65, 88, 30, 100]
+var stoneValues = [1,2,3,4,5,6,7,8,9,10,11,12]
 
-var stoneValues = Math.floor(Math.random() * 13)
-console.log(stoneValues);
+var guessedNumber = targetNumbers[Math.floor(Math.random() * targetNumbers.length)];
+var stoneValue = stoneValues[Math.floor(Math.random() * stoneValues.length)];
 
-$("guessed-number").text(targetNumber)
+$(".guessed-number").text(guessedNumber)
+$(".total-number").text(counter)
 
 
 
 function startGame(){
+    
     var counter = 0;
     $("#stone1").on("click", function() {
-        counter +=1
+        counter +=stoneValue;
     });
     
     $("#stone2").on("click", function() {
-        counter +=1
+        counter +=stoneValue;
     });
     
     $("#stone3").on("click", function() {
-        counter +=1
-    });
+        counter +=stoneValue;
+    })
     
     $("#stone4").on("click", function() {
-        counter +=1
+        counter +=stoneValue;
     });
+
 if (counter === targetNumber) {
     alert("You win. The universe is saved!!");
+    wins++;
+    $(".wins").html("Wins: " + " " + wins)
+    startGame()
+
 } else if (counter >= targetNumber) {
-    alert("Thanos has all of the infinity stones!!!");
+    alert("You lost. Thanos has acquired annother Infinity stone!!!!");
+    losses++;
+    $(".losses").html("Losses: " + " " + losses)
+    startGame()
 }
 
-
+startGame()
 
 
 
