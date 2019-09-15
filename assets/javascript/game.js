b81d6e129dd811a0c883b4f0f2485c65e3302932
SOVEREIGN <isimemeni@gmail.com>
2019-09-14 21:46:21 -0400

    corrected random number functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,55 @@
 var wins = 0;
 var losses = 0;
-var targetNumbers = [40, 55, 79, 120, 20, 65, 88, 30, 100]
 var stoneValues = [1,2,3,4,5,6,7,8,9,10,11,12]
-console.log(targetNumbers);
 var counter = 0
-var guessedNumber = targetNumbers[Math.floor(Math.random() * targetNumbers.length)];
-var stoneValue = stoneValues[Math.floor(Math.random() * stoneValues.length)];
-console.log(guessedNumber)
-console.log(stoneValue)
 
-$(".guessed-number").html(guessedNumber);
 $(".total-number").html(counter);
 $(".wins").html("Wins: " + " " + wins)
 $(".losses").html("Losses: " + " " + losses)
 
-    function startGame(){
+function startGame(){
+    var guessedNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+    console.log(guessedNumber)
+
+    $(".guessed-number").html(guessedNumber);
     wins = 0;
     losses = 0;  
     counter = 0;
+    var stoneValueOne = Math.floor(Math.random() * 12) + 1;
+    var stoneValueTwo = Math.floor(Math.random() * 12) + 1;
+    var stoneValueThree = Math.floor(Math.random() * 12) + 1;
+    var stoneValueFour = Math.floor(Math.random() * 12) + 1;
 
-stoneValue = parseInt(stoneValue);
 
     $("#stone1").on("click", function() {
-        stoneValue = stoneValues[Math.floor(Math.random() * stoneValues.length)];
-        counter +=stoneValue;
-        alert("Total score increased by" + " " + stoneValue)
-    });
+        counter += stoneValueOne;
+        alert("Total score increased to" + " " + counter);
+        $(".total-number").html(counter);
+        console.log(stoneValueOne)
 
+    });
+    
     $("#stone2").on("click", function() {
-        stoneValue = stoneValues[Math.floor(Math.random() * stoneValues.length)];
-        counter +=stoneValue;
-        alert("Total score increased by" + " " + stoneValue)
+        counter +=stoneValueTwo;
+        alert("Total score increased to" + " " + counter);
+        $(".total-number").html(counter);
+        console.log(stoneValueTwo)
     });
     
     $("#stone3").on("click", function() {
-        stoneValue = stoneValues[Math.floor(Math.random() * stoneValues.length)];
-        counter +=stoneValue;
-        alert("Total score increased by" + " " + stoneValue)
-    })
-    
-    $("#stone4").on("click", function() {
-        stoneValue = stoneValues[Math.floor(Math.random() * stoneValues.length)];
-        counter +=stoneValue;
-        alert("Total score increased by" + " " + stoneValue)
+        counter +=stoneValueThree;
+        alert("Total score increased to" + " " + counter);
+        $(".total-number").html(counter);
+        console.log(stoneValueThree)
     });
+    
+    $("#stone4").on("click", function() { 
+        counter +=stoneValueFour;
+        alert("Total score increased to" + " " + counter);
+        $(".total-number").html(counter);
+        console.log(stoneValueFour)
+    });
+
 
 if (counter === guessedNumber) {
     alert("You win. The universe is saved!!");
@@ -59,7 +65,5 @@ if (counter === guessedNumber) {
 }
 
 }
-
-
 startGame()
 
