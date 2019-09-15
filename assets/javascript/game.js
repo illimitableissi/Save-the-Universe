db9730e84c0aa5eae624dc5b254e260c5db28050
SOVEREIGN <isimemeni@gmail.com>
2019-09-14 20:27:07 -0400

    on click functions now provide seperate random numbers for each pic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,65 @@
+var wins = 0;
+var losses = 0;
 var targetNumbers = [40, 55, 79, 120, 20, 65, 88, 30, 100]
 var stoneValues = [1,2,3,4,5,6,7,8,9,10,11,12]
-debugger;
-
+console.log(targetNumbers);
+var counter = 0
 var guessedNumber = targetNumbers[Math.floor(Math.random() * targetNumbers.length)];
 var stoneValue = stoneValues[Math.floor(Math.random() * stoneValues.length)];
+console.log(guessedNumber)
+console.log(stoneValue)
 
-$(".guessed-number").html(guessedNumber)
-$(".total-number").text(counter)
+$(".guessed-number").html(guessedNumber);
+$(".total-number").html(counter);
+$(".wins").html("Wins: " + " " + wins)
+$(".losses").html("Losses: " + " " + losses)
 
+    function startGame(){
+    wins = 0;
+    losses = 0;  
+    counter = 0;
 
+stoneValue = parseInt(stoneValue);
 
-function startGame(){
-    
-    var counter = 0;
     $("#stone1").on("click", function() {
+        stoneValue = stoneValues[Math.floor(Math.random() * stoneValues.length)];
         counter +=stoneValue;
+        alert("Total score increased by" + " " + stoneValue)
     });
-    
+
     $("#stone2").on("click", function() {
+        stoneValue = stoneValues[Math.floor(Math.random() * stoneValues.length)];
         counter +=stoneValue;
+        alert("Total score increased by" + " " + stoneValue)
     });
     
     $("#stone3").on("click", function() {
+        stoneValue = stoneValues[Math.floor(Math.random() * stoneValues.length)];
         counter +=stoneValue;
+        alert("Total score increased by" + " " + stoneValue)
     })
     
     $("#stone4").on("click", function() {
+        stoneValue = stoneValues[Math.floor(Math.random() * stoneValues.length)];
         counter +=stoneValue;
+        alert("Total score increased by" + " " + stoneValue)
     });
 
-if (counter === targetNumber) {
+if (counter === guessedNumber) {
     alert("You win. The universe is saved!!");
     wins++;
-    $(".wins").html("Wins: " + " " + wins)
     startGame()
+    $(".wins").html("Wins: " + " " + wins)
 
-} else if (counter >= targetNumber) {
+} else if (counter >= guessedNumber) {
     alert("You lost. Thanos has acquired annother Infinity stone!!!!");
     losses++;
-    $(".losses").html("Losses: " + " " + losses)
     startGame()
-}
+    $(".losses").html("Losses: " + " " + losses)
 }
 
+}
+
+
 startGame()
+
