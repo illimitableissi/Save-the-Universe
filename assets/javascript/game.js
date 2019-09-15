a18053506cf30f978e313cfd23f8b5bbfceaefef
SOVEREIGN <isimemeni@gmail.com>
2019-09-15 08:10:06 -0400

    reset function now updates are variables completely

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,19 @@
 var wins = 0;
 var losses = 0;
-var stoneValues = [1,2,3,4,5,6,7,8,9,10,11,12]
-var counter = 0
+var counter = 0;
+var stoneValueOne = Math.floor(Math.random() * 12) + 1;
+var stoneValueTwo = Math.floor(Math.random() * 12) + 1;
+var stoneValueThree = Math.floor(Math.random() * 12) + 1;
+var stoneValueFour = Math.floor(Math.random() * 12) + 1;
+var guessedNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+
 
 $(".total-number").html(counter);
 $(".wins").html("Wins: " + " " + wins)
 $(".losses").html("Losses: " + " " + losses)
 
-function startGame(){
+$(document).ready(function() {
+
     var guessedNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
     console.log(guessedNumber)
 
@@ -25,7 +31,21 @@ function startGame(){
         counter += stoneValueOne;
         alert("Total score increased to" + " " + counter);
         $(".total-number").html(counter);
-        console.log(stoneValueOne)
+        console.log(stoneValueOne);
+        if (counter === guessedNumber) {
+            alert("You win. The universe is saved!!");
+            wins++;
+            reset();
+            $(".wins").html("Wins: " + " " + wins);
+            
+        } else if (counter >= guessedNumber) {
+            alert("You lost. Thanos has acquired annother Infinity stone!!!!");
+            losses++;
+            reset();
+            $(".losses").html("Losses: " + " " + losses)
+            
+        }
+        
 
     });
     
@@ -33,37 +53,77 @@ function startGame(){
         counter +=stoneValueTwo;
         alert("Total score increased to" + " " + counter);
         $(".total-number").html(counter);
-        console.log(stoneValueTwo)
+        console.log(stoneValueTwo);
+        if (counter === guessedNumber) {
+            alert("You win. The universe is saved!!");
+            wins++;
+            reset();
+            $(".wins").html("Wins: " + " " + wins)
+        } else if (counter >= guessedNumber) {
+            alert("You lost. Thanos has acquired another Infinity stone!!!!");
+            losses++;
+            reset();
+            $(".losses").html("Losses: " + " " + losses)
+            
+        }
+        
     });
     
     $("#stone3").on("click", function() {
         counter +=stoneValueThree;
         alert("Total score increased to" + " " + counter);
         $(".total-number").html(counter);
-        console.log(stoneValueThree)
+        console.log(stoneValueThree);
+        if (counter === guessedNumber) {
+            alert("You win. The universe is saved!!");
+            wins++;
+            reset();
+            $(".wins").html("Wins: " + " " + wins);
+            
+        } else if (counter >= guessedNumber) {
+            alert("You lost. Thanos has acquired another Infinity stone!!!!");
+            losses++;
+            reset();
+            $(".losses").html("Losses: " + " " + losses);
+            
+        }
+        
     });
     
     $("#stone4").on("click", function() { 
         counter +=stoneValueFour;
         alert("Total score increased to" + " " + counter);
         $(".total-number").html(counter);
-        console.log(stoneValueFour)
+        console.log(stoneValueFour);
+        if (counter === guessedNumber) {
+            alert("You win. The universe is saved!!");
+            wins++;
+            reset();
+            $(".wins").html("Wins: " + " " + wins)
+            
+        } else if (counter >= guessedNumber) {
+            alert("You lost. Thanos has acquired another Infinity stone!!!!");
+            losses++;
+            $(".losses").html("Losses: " + " " + losses);
+            reset();
+            
+        }
+        
     });
 
 
-if (counter === guessedNumber) {
-    alert("You win. The universe is saved!!");
-    wins++;
-    startGame()
-    $(".wins").html("Wins: " + " " + wins)
+function reset() {
 
-} else if (counter >= guessedNumber) {
-    alert("You lost. Thanos has acquired annother Infinity stone!!!!");
-    losses++;
-    startGame()
-    $(".losses").html("Losses: " + " " + losses)
-}
+guessedNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+console.log(guessedNumber)
+$(".guessed-number").html(guessedNumber);
+counter = 0;
+stoneValueOne = Math.floor(Math.random() * 12) + 1;
+stoneValueTwo = Math.floor(Math.random() * 12) + 1;
+stoneValueThree = Math.floor(Math.random() * 12) + 1;
+stoneValueFour = Math.floor(Math.random() * 12) + 1;
 
 }
-startGame()
 
+
+});
